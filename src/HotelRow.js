3626f273f30d493ce9445dddbbeb3d295dd480c3
Yoni Weisbrod <yoni@fastmail.com>
2017-03-19 07:14:47 +0200

    add listview and empty hotelrow

---
+++ src/HotelRow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet, View } from 'react-native'
+
+class HotelRow extends Component {
+	render() {
+		return (
+			<View>
+			</View>
+		)
+	}
+}
+
+
+export { HotelRow as default };
--- src/ResultsList.js
+++ src/ResultsList.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, ListView } from 'react-native'
+import { Text, StyleSheet, View, ListView, Image } from 'react-native'
+import HotelRow from './HotelRow';
 
 class ResultsList extends Component {
 	static PropTypes = {
 		data: React.PropTypes.array
 	}
-	
+
+	constructor(props) {
+		super(props);
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		this.state = {
+			dataSource: ds.cloneWithRows(this.props.data)
+		}
+	}
+
+	_renderHeader = (rowData) => (
+		<Image
+      source={require('../assets/images/nyhotelmap.jpeg')}
+      style={{ height: 150 }}  />
+	)
+
+	_renderRow = (rowData) => (
+		<HotelRow data={ rowData } />
+	)
+
+	_renderSeparator = (sectionId, rowId) => (
+		<View 
+			style={{height: 3}}
+			key={rowId}>
+		</View>
+	)
+
 	render() {
 		return (
-			<View style={{flex: 0.9, backgroundColor: 'blue'}}>
-
+			<View style={{flex: 0.9}}>
 				<ListView
-					style={ styles.mainList }
 					dataSource={ this.state.dataSource }
-					renderRow={ this.rowRenderer }
-					renderHeader={ this.headerRenderer }
-					renderSeparator={ this.separatorRenderer }
+					renderRow={ this._renderRow }
+					renderHeader={ this._renderHeader }
+					renderSeparator={ this._renderSeparator }
 				/>
-
 			</View>
 		)
 	}
