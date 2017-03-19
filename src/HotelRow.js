0311ea519d19417cf13917ad8b82eacb6868c4c9
Yoni Weisbrod <yoni@fastmail.com>
2017-03-19 07:36:57 +0200

    basic row style

--- src/HotelRow.js
+++ src/HotelRow.js
@@ -7,7 +7,21 @@ class HotelRow extends Component {
 			<Image
 				source={require('../assets/images/hotelfront.jpg')}
 				style={{ width: null, height: 200 }}>
-				<Text>Test me</Text>
+				<View style={{flex: 1, justifyContent: 'space-between', backgroundColor: 'transparent', paddingHorizontal: 10}}>
+					<View style={{flex: 0.7}}>
+						<Text style={{color: 'white'}}>29 people viewing this property.</Text>
+					</View>
+					<View style={{flex: 0.3, flexDirection: 'row', justifyContent: 'space-between'}}>
+						<View>
+							<Text style={{color: 'white'}}>One</Text>
+							<Text style={{color: 'white'}}>Two</Text>
+							<Text style={{color: 'white'}}>Three</Text>
+						</View>
+						<View>
+							<Text style={{color: 'white'}}>Price goes here</Text>
+						</View>
+					</View>
+				</View>
 			</Image>
 		)
 	}
